972308933f4f9d382f6d45e9b6c156d5f5177149
OzmaKA <154746883+OzmaKa@users.noreply.github.com>
2026-03-19 12:52:55 +0300

    sys prompt update

--- BackEnd/aiService/aiService.ts
+++ BackEnd/aiService/aiService.ts
@@ -1,32 +1,40 @@
-export const translateCode = async (from: string, to: string, code: string): Promise<string> => {
-    
-    const apiKey = process.env.COHERE_API_KEY!;
+export const translateCode = async (
+  from: string,
+  to: string,
+  code: string,
+): Promise<string> => {
+  const apiKey = process.env.COHERE_API_KEY!;
 
-    const response = await fetch(
-        "https://api.cohere.com/v2/chat",
+  const response = await fetch("https://api.cohere.com/v2/chat", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "command-r-plus-08-2024",
+      messages: [
         {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${apiKey}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                model: "command-r-plus-08-2024",
-                messages: [
-                    {
-                        role: "user",
-                        content: `Convert this code from ${from} to ${to}.
-                                  Return only the converted code with no explanation,
-                                  no markdown, no code blocks.
-                                  Preserve the exact same logic and behavior.
-                                  Code:
-                                  ${code}`
-                    }
-                ]
-            })
-        }
-    );
+          role: "system",
+          content: "you are a code coversion engine
+                        your only job is to convert code from one programming language to another
+                        Rules: 
+                        - Return only the converted code
+- No explanations
+- No markdown formatting  
+- No code blocks or backticks
+- Preserve exact logic and behavior
+- If the input is not valid code, return: ERROR: Invalid code input",
+        },
 
-    const data = await response.json();
-    return data.message.content[0].text;
-}
+        {
+          role: "user",
+          content: `Convert this code from ${from} to ${to}:\n\n${code}`,
+        },
+      ],
+    }),
+  });
+
+  const data = await response.json();
+  return data.message.content[0].text;
+};
